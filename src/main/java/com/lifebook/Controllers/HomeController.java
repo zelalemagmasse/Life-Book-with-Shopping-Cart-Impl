f73a99a2fa70bf6e2e02b115bcd25a26437c98e3
GBTC406010UR <GBTC406010UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-17 14:13:52 -0400

    home controller added

--- src/main/java/com/lifebook/Controllers/HomeController.java
+++ src/main/java/com/lifebook/Controllers/HomeController.java
@@ -1,4 +1,43 @@
 package com.lifebook.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage() {
+
+        return "index(anyone)";
+    }
+
+	@GetMapping("/login")
+	public String login() {
+
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String loggedIn() {
+
+		return "redirect:/account/";
+	}
+
+	@GetMapping("/register")
+	public String registration(){
+
+		return "registration";
+	}
+
+	@PostMapping("/register")
+	public String completeRegistration() {
+
+		// Save new user here
+
+		return "login";
+	}
+
 }
