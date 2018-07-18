533f125cf615fffbc10e6df9f7cb05c625e21b9d
konstantin <konstantin_gulish@yahoo.com>
2018-07-18 09:48:36 -0400

    updated

--- src/main/java/com/lifebook/Controllers/HomeController.java
+++ src/main/java/com/lifebook/Controllers/HomeController.java
@@ -1,5 +1,9 @@
 package com.lifebook.Controllers;
 
+import com.lifebook.Model.AppRole;
+import com.lifebook.Model.AppUser;
+import com.lifebook.Repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +14,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String homePage() {
+        
         return "index";
     }
 
